1bf3be05398e5e5f2b48dc44cf621ce8362a5a5d
Aryan Patel <21cs038@charusat.edu.in>
2024-04-27 10:52:33 +0530

    All Ohk

--- client/src/components/loader/Loader.js
+++ client/src/components/loader/Loader.js
@@ -4,13 +4,12 @@ import ReactDOM from "react-dom";
 import "./Loader.scss";
 
 const Loader = () => {
-  return ReactDOM.createPortal(
+  return (
     <div className="wrapper">
       <div className="loader">
         <img src={loaderImg} alt="Loading..." />
       </div>
-    </div>,
-    document.getElementById("loader")
+    </div>
   );
 };
 
